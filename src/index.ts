82f903a89eb51e54ca1cb15e3d5b4705d4ec5b2d
Thierry de Pauw <thierry@thinkinglabs.io>
2026-03-25 21:29:11 +0100

    Remove code

--- src/index.ts
+++
@@ -1,8 +0,0 @@
-
-export function sum(a: number, b: number) {
-  return a + b;
-}
-
-export function subtract(a: number, b: number) {
-  return a - b;
-}
--- test/subtract.test.ts
+++
@@ -1,5 +0,0 @@
-import {subtract} from '../src/index';
-
-test('substract 2 - 1 to equal 1', () => {
-  expect(subtract(2, 1)).toBe(1);
-});
